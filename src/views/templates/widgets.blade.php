f8783c0aa34f6ab0b847d95f2dad1fc107c84e22
Jay Hariani <jhariani@gmail.com>
2015-10-28 09:59:07 -0400

    Update widgets.blade.php

--- src/views/templates/widgets.blade.php
+++ src/views/templates/widgets.blade.php
@@ -130,71 +130,73 @@
 
 			<img border="0" src="{{ Request::getSchemeAndHttpHost() }}/vendor/beautymail/assets/images/widgets/spacer.gif" width="1" height="25" class="divider"><br>
 
-			<table width="100%" cellpadding="0" cellspacing="0" border="0" bgcolor="#f2f2f2">
-				<tr>
-					<td>
+			@if (!isset($hideFooter))
+				<table width="100%" cellpadding="0" cellspacing="0" border="0" bgcolor="#f2f2f2">
+					<tr>
+						<td>
 
-						<img border="0" src="{{ Request::getSchemeAndHttpHost() }}/vendor/beautymail/assets/images/widgets/spacer.gif" width="1" height="30"><br>
+							<img border="0" src="{{ Request::getSchemeAndHttpHost() }}/vendor/beautymail/assets/images/widgets/spacer.gif" width="1" height="30"><br>
 
-						<table width="600" cellpadding="0" cellspacing="0" border="0" align="center" class="table">
-							<tr>
-								<td width="600" nowrap bgcolor="#f2f2f2" class="cell">
+							<table width="600" cellpadding="0" cellspacing="0" border="0" align="center" class="table">
+								<tr>
+									<td width="600" nowrap bgcolor="#f2f2f2" class="cell">
 
-									<table width="600" cellpadding="0" cellspacing="0" border="0" class="table">
-										<tr>
-											<td width="380" valign="top" class="footershow">
+										<table width="600" cellpadding="0" cellspacing="0" border="0" class="table">
+											<tr>
+												<td width="380" valign="top" class="footershow">
 
-												<img border="0" src="{{ Request::getSchemeAndHttpHost() }}/vendor/beautymail/assets/images/widgets/spacer.gif" width="1" height="8"><br>
+													<img border="0" src="{{ Request::getSchemeAndHttpHost() }}/vendor/beautymail/assets/images/widgets/spacer.gif" width="1" height="8"><br>
 
-												@if (isset($reminder))
-													<p style="color:#a6a6a6;font-size:12px;font-family:Helvetica,Arial,sans-serif;margin-top:0;margin-bottom:15px;padding-top:0;padding-bottom:0;line-height:18px;" class="reminder">{!! $reminder  !!}</p>
-												@endif
+													@if (isset($reminder))
+														<p style="color:#a6a6a6;font-size:12px;font-family:Helvetica,Arial,sans-serif;margin-top:0;margin-bottom:15px;padding-top:0;padding-bottom:0;line-height:18px;" class="reminder">{!! $reminder  !!}</p>
+													@endif
 
-												@if (isset($unsubscribe))
-													<p style="color:#c9c9c9;font-size:12px;font-family:'Helvetica Neue',Helvetica,Arial,sans-serif;">{!! $unsubscribe !!}</p>
-												@endif
+													@if (isset($unsubscribe))
+														<p style="color:#c9c9c9;font-size:12px;font-family:'Helvetica Neue',Helvetica,Arial,sans-serif;">{!! $unsubscribe !!}</p>
+													@endif
 
-											</td>
-											<td align="right" width="220" style="color:#a6a6a6;font-size:12px;font-family:'Helvetica Neue',Helvetica,Arial,sans-serif;text-shadow: 0 1px 0 #ffffff;" valign="top" class="hide">
+												</td>
+												<td align="right" width="220" style="color:#a6a6a6;font-size:12px;font-family:'Helvetica Neue',Helvetica,Arial,sans-serif;text-shadow: 0 1px 0 #ffffff;" valign="top" class="hide">
 
-												<table cellpadding="0" cellspacing="0" border="0">
-													<tr>
-														@if (isset($flickr))
-															<td><a href="{{ $flickr }}"><img border="0" src="{{ Request::getSchemeAndHttpHost() }}/vendor/beautymail/assets/images/widgets/flickr.gif" width="42" height="32" alt="See our photos on Flickr"></a></td>
-														@endif
+													<table cellpadding="0" cellspacing="0" border="0">
+														<tr>
+															@if (isset($flickr))
+																<td><a href="{{ $flickr }}"><img border="0" src="{{ Request::getSchemeAndHttpHost() }}/vendor/beautymail/assets/images/widgets/flickr.gif" width="42" height="32" alt="See our photos on Flickr"></a></td>
+															@endif
 
-														@if (isset($twitter))
-															<td><a href="{{ $twitter }}"><img border="0" src="{{ Request::getSchemeAndHttpHost() }}/vendor/beautymail/assets/images/widgets/twitter.gif" width="42" height="32" alt="Follow us on Twitter"></a></td>
-														@endif
+															@if (isset($twitter))
+																<td><a href="{{ $twitter }}"><img border="0" src="{{ Request::getSchemeAndHttpHost() }}/vendor/beautymail/assets/images/widgets/twitter.gif" width="42" height="32" alt="Follow us on Twitter"></a></td>
+															@endif
 
-														@if (isset($facebook))
-															<td><a href="{{ $facebook }}"><img border="0" src="{{ Request::getSchemeAndHttpHost() }}/vendor/beautymail/assets/images/widgets/facebook.gif" width="32" height="32" alt="Visit us on Facebook"></a></td>
-														@endif
+															@if (isset($facebook))
+																<td><a href="{{ $facebook }}"><img border="0" src="{{ Request::getSchemeAndHttpHost() }}/vendor/beautymail/assets/images/widgets/facebook.gif" width="32" height="32" alt="Visit us on Facebook"></a></td>
+															@endif
 
-													</tr>
-												</table>
+														</tr>
+													</table>
 
-												<img border="0" src="{{ Request::getSchemeAndHttpHost() }}/vendor/beautymail/assets/images/widgets/spacer.gif" width="1" height="10"><br><p style="color:#b3b3b3;font-size:11px;line-height:15px;font-family:Helvetica,Arial,sans-serif;margin-top:0;margin-bottom:0;padding-top:0;padding-bottom:0;font-weight:bold;">{{ $senderName or '' }}</p>
+													<img border="0" src="{{ Request::getSchemeAndHttpHost() }}/vendor/beautymail/assets/images/widgets/spacer.gif" width="1" height="10"><br><p style="color:#b3b3b3;font-size:11px;line-height:15px;font-family:Helvetica,Arial,sans-serif;margin-top:0;margin-bottom:0;padding-top:0;padding-bottom:0;font-weight:bold;">{{ $senderName or '' }}</p>
 
-												@if (isset($address))
-													<p style="color:#b3b3b3;font-size:11px;line-height:15px;font-family:Helvetica,Arial,sans-serif;margin-top:0;margin-bottom:0;padding-top:0;padding-bottom:0;font-weight:normal;">
-														{{ $address }}
-													</p>
-												@endif
+													@if (isset($address))
+														<p style="color:#b3b3b3;font-size:11px;line-height:15px;font-family:Helvetica,Arial,sans-serif;margin-top:0;margin-bottom:0;padding-top:0;padding-bottom:0;font-weight:normal;">
+															{{ $address }}
+														</p>
+													@endif
 
-											</td>
-										</tr>
-									</table>
+												</td>
+											</tr>
+										</table>
 
-								</td>
-							</tr>
-						</table>
+									</td>
+								</tr>
+							</table>
 
-						<img border="0" src="{{ Request::getSchemeAndHttpHost() }}/vendor/beautymail/assets/images/widgets/spacer.gif" width="1" height="25"><br>
+							<img border="0" src="{{ Request::getSchemeAndHttpHost() }}/vendor/beautymail/assets/images/widgets/spacer.gif" width="1" height="25"><br>
 
-					</td>
-				</tr>
-			</table>
+						</td>
+					</tr>
+				</table>
+			@endif
 
 		</td>
 	</tr>
